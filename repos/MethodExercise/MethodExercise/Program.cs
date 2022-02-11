9a6c55ad0c651d880e24056a60d87497c50eb657
CheyNel5 <cnelson9005@gmail.com>
2022-02-11 14:30:01 -0500

    began user questions

---
+++ repos/MethodExercise/MethodExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //---------------Exercise 1----------------------------------------
+            //Name: Cheyenne
+            //Favorite Color: Green
+            //Favorite Animal: Tiger
+            //Favorite Band: Goo Goo Dolls
+
+            Console.WriteLine("Hello - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. What is your favorite color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an awesome color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! Now, what is your favorite band?");
+            var band = Console.ReadLine();
+
+        }
+    }
+}
