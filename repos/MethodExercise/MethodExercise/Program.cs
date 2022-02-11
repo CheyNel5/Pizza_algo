660fab0db4ab7b47c6b029a47dd1e2ddb67fbfbc
CheyNel5 <cnelson9005@gmail.com>
2022-02-11 15:07:36 -0500

    initial commit

--- repos/MethodExercise/MethodExercise/Program.cs
+++ repos/MethodExercise/MethodExercise/Program.cs
@@ -10,10 +10,16 @@ namespace MethodExercise
             return answer;
         }
 
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+
         static void Main(string[] args)
         {
 
             var amountOfCars = Sum(2, 6);
+            var blah = Multiply(60, 2, 4);
 
 
             //---------------Exercise 1----------------------------------------
